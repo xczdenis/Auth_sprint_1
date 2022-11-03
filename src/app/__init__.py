ac4382950999f6f90071ba3414c5f5f0b69f196e
Denis <xczdenis@gmail.com>
2022-11-03 11:03:33 +0300

    feat: добавлен jaeger

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from app.jaeger import JaegerManager
 from app.swagger import swagger_config, swagger_template
 from config import settings
 
@@ -14,16 +15,17 @@ migrate = Migrate()
 jwt = JWTManager()
 jwt_redis_blocklist = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
 
+jaeger = JaegerManager(host=settings.JAEGER_HOST, port=settings.JAEGER_UDP_PORT)
+
+swagger = Swagger()
+swagger.config.update(swagger_config)
+
 
 @jwt.token_in_blocklist_loader
 def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
     jti = jwt_payload["jti"]
     token_in_redis = jwt_redis_blocklist.get(jti)
     return token_in_redis is not None
-
-
-swagger = Swagger()
-swagger.config.update(swagger_config)
 
 
 def create_app():
@@ -34,6 +36,8 @@ def create_app():
     migrate.init_app(app, db)
 
     jwt.init_app(app)
+
+    jaeger.init_app(app)
 
     swagger.init_app(app)
     swagger.template = swagger_template
