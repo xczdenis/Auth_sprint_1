9379d87dbbdd30e93bf93c0f44fe925e8b66b8b1
Denis <xczdenis@gmail.com>
2022-11-17 11:32:46 +0300

    refactor: параметры провайдеров отправлены в настройки

--- src/app/oauth2/utils.py
+++ src/app/oauth2/utils.py
@@ -1,4 +1,18 @@
+from loguru import logger
+
 from app.utils import get_parameter_from_request
+from config import settings
+
+
+def get_provider_attr_from_settings(provider_env_prefix: str, attr_name: str):
+    value = ""
+    env_name = f"{provider_env_prefix}_oauth".upper()
+    try:
+        provider_settings = getattr(settings, env_name)
+        value = getattr(provider_settings, attr_name)
+    except AttributeError as e:
+        logger.error(e)
+    return value
 
 
 def get_provider_from_request(**kwargs):
